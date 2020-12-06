a5d83bc2772b7cef7a2f60e4eb23007cb6052276
Ramyanee <almendra.rk@gmail.com>
2020-12-07 00:17:30 +0530

    heroku config

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -26,8 +26,7 @@ SECRET_KEY = '7sv8u8%!ah3reqzmp@-fwysw37zrhh4cwb_sb88ce(c^zo&$qu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','mybotrasa.herokuapp.com/']
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -122,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
